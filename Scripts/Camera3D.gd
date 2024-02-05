b2c8d51f102be9519754d5a218e9639c04a2f695
Myles <mylesleslie2@gmail.com>
2024-02-05 21:32:36 +0000

    Fixed the player camera
    - fixed camera
    - tweaked GO's
    - Project management bits

---
+++ Scripts/Camera3D.gd
@@ -0,0 +1,20 @@
+extends Camera3D
+
+@onready var target = $"../Player/Truck/truck_grey"
+
+@export var follow_speed = 100.0
+@export var offset_y = Vector3.ZERO
+@export var offset_z = Vector3.ZERO
+@export var lerp_speed = 3.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if !target:
+		return
+	var target_pos = target.global_transform.translated_local(offset_y + -offset_z)
+	global_transform = global_transform.interpolate_with(target_pos, lerp_speed * delta)
+	look_at(target.global_position, Vector3.UP)
