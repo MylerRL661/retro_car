7ff6f756348815ae083b4608c26e62438ecae88d
Myles <mylesleslie2@gmail.com>
2024-02-12 22:09:47 +0000

    tried signalling and grid maps
    -need to find more resources online

--- Prefabs/truck_green.gd
+++ Prefabs/truck_green.gd
@@ -3,16 +3,12 @@ extends RigidBody3D
 var min_flying_force: float = 500.0
 var max_flying_force: float = 1500.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$".".body_entered.connect(_on_Player_body_entered)
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _on_Player_body_entered(body: Node) -> void:
+func _on_body_entered(body):
+	print("Somethings at the door")
 	if body.is_in_group("player") and body is RigidBody3D:
 		print('SMASHED - Signal working')
 		var direction = (body.global_transform.origin - global_transform.origin).normalized()
