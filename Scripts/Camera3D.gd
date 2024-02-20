ece22363563a4cb71153654907f9d34d3d50c2ae
MLeslie-Retinize <myles@retinize.com>
2024-02-20 13:10:29 +0000

    Small updates
    Made skybox more pixelated
    Played around with the camera to make it feel better - still in progress
    Began adding particles

--- Scripts/Camera3D.gd
+++ Scripts/Camera3D.gd
@@ -18,3 +18,4 @@ func _physics_process(delta):
 	var target_pos = target.global_transform.translated_local(offset_y + -offset_z)
 	global_transform = global_transform.interpolate_with(target_pos, lerp_speed * delta)
 	look_at(target.global_position, Vector3.UP)
+	print("Cam position: ", global_position)
