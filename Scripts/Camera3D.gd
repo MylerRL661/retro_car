8f9f21681ccb11bec61a25cd85dc0d9d7ce200f1
Myles <mylesleslie2@gmail.com>
2024-03-25 11:57:05 +0000

    Update Camera3D.gd

--- Scripts/Camera3D.gd
+++ Scripts/Camera3D.gd
@@ -5,7 +5,7 @@ extends Camera3D
 @export var follow_speed = 100.0
 @export var offset_y = Vector3.ZERO
 @export var offset_z = Vector3.ZERO
-@export var lerp_speed = 1.5
+@export var lerp_speed = 2
 @export var cam_logging = false
 
 # Called when the node enters the scene tree for the first time.
